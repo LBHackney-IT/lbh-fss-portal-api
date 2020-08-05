9de40048b714fb8f1da6d9c6a000e086807ca232
Martin Currie <martin.currie@nudgedigital.co.uk>
2020-08-05 21:40:53 +0100

    Used Renamer powershell script to change filenames/code from BaseApi to LBHFSSPortalApi

---
+++ LBHFSSPortalAPI/V1/UseCase/Interfaces/IGetAllUseCase.cs
@@ -0,0 +1,9 @@
+using LBHFSSPortalAPI.V1.Boundary.Response;
+
+namespace LBHFSSPortalAPI.V1.UseCase.Interfaces
+{
+    public interface IGetAllUseCase
+    {
+        ResponseObjectList Execute();
+    }
+}
