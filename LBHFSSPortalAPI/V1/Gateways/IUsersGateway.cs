3a30a8f2c4755f1bfd7a25a97ac1654ec64a8df2
Martin Currie <martin.currie@nudgedigital.co.uk>
2020-08-14 11:59:49 +0100

    removed redundant code

--- LBHFSSPortalAPI/V1/Gateways/IUsersGateway.cs
+++ LBHFSSPortalAPI/V1/Gateways/IUsersGateway.cs
@@ -1,14 +1,10 @@
 using LBHFSSPortalAPI.V1.Domain;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LBHFSSPortalAPI.V1.Gateways
 {
     public interface IUsersGateway
     {
         List<UserDomain> GetAllUsers();
-        //ResidentDomain GetResidentById(int id);
     }
 }
