da6ccaabacb07aafb19e89c42ba755ec260b81ab
Martin Currie <martin.currie@nudgedigital.co.uk>
2020-08-18 17:47:50 +0100

    Added Cognito Extensions package and test code to log on user

--- LBHFSSPortalAPI/V1/Boundary/Requests/LoginUserQueryParam.cs
+++ LBHFSSPortalAPI/V1/Boundary/Requests/LoginUserQueryParam.cs
@@ -4,8 +4,8 @@ namespace LBHFSSPortalAPI.V1.Boundary.Requests
 {
     public class LoginUserQueryParam
     {
-        [FromQuery(Name = "username")]
-        public string Username { get; set; }
+        [FromQuery(Name = "emailaddress")]
+        public string EmailAddress { get; set; }
 
         [FromQuery(Name = "password")]
         public string Password { get; set; }
--- LBHFSSPortalAPI/V1/Gateways/AuthenticateGateway.cs
+++ LBHFSSPortalAPI/V1/Gateways/AuthenticateGateway.cs
@@ -1,14 +1,33 @@
 using LBHFSSPortalAPI.V1.Boundary.Requests;
 using LBHFSSPortalAPI.V1.Domain;
-//using Amazon.CognitoIdentity;
-//using Amazon.S3;
+using Amazon.CognitoIdentityProvider;
+using Amazon.Extensions.CognitoAuthentication;
+using Amazon.Runtime;
+using Amazon.CognitoIdentity;
 
 namespace LBHFSSPortalAPI.V1.Gateways
 {
     public class AuthenticateGateway : IAuthenticateGateway
     {
+        private const string ClientId = "3ogiujoo72poudged2amgiefss";
+        private const string PoolId = "7uam787ffful1egjdiqveaeaajvbnmssrud6v1lu16t0ma2i288";
+        //private const string ClientSecret = "7uam787ffful1egjdiqveaeaajvbnmssrud6v1lu16t0ma2i288";
+
         public void CreateUser(LoginUserQueryParam loginUserQueryParam)
         {
+
+            //CognitoAWSCredentials credentials = new CognitoAWSCredentials(
+            //    accountId,        // Account number
+            //    identityPoolId,   // Identity pool ID
+            //    unAuthRoleArn,    // Role for unauthenticated users
+            //    null,             // Role for authenticated users, not set
+            //    region);
+
+            //using (var s3Client = new AmazonS3Client(credentials))
+            //{
+            //    s3Client.ListBuckets();
+            //}
+
             throw new System.NotImplementedException();
         }
 
@@ -17,23 +36,24 @@ namespace LBHFSSPortalAPI.V1.Gateways
             // TODO (MJC) Use the query params 'username' and 'password' to log into AWS Cognito
             // and store 'user pool token' (sub_id) in the portal api Users database 
 
-            //+++
-            // Amazon Cognito Credentials Provider
-            //---
+            CognitoUserPool userPool = null;
+            string accessToken = string.Empty;
 
-            //CognitoAWSCredentials credentials = new CognitoAWSCredentials(
-            //                accountId,        // Account number
-            //                identityPoolId,   // Identity pool ID
-            //                unAuthRoleArn,    // Role for unauthenticated users
-            //                null,             // Role for authenticated users, not set
-            //                region);
+            using (var provider = new AmazonCognitoIdentityProviderClient(new AnonymousAWSCredentials()))
+            {
+                userPool = new CognitoUserPool(PoolId, ClientId, provider);
 
-            //using (var s3Client = new Amazon.S3.AmazonS3Client(credentials))
-            //{
-            //    s3Client.ListBuckets();
-            //}
+                CognitoUser user = new CognitoUser(loginUserQueryParam.EmailAddress, "clientID", userPool, provider);
+                InitiateSrpAuthRequest authRequest = new InitiateSrpAuthRequest()
+                {
+                    Password = loginUserQueryParam.Password
+                };
 
-            throw new System.NotImplementedException();
+                AuthFlowResponse authResponse = user.StartWithSrpAuthAsync(authRequest).Result;
+                accessToken = authResponse.AuthenticationResult.AccessToken;
+            }
+
+            return new LoginDomain();
         }
     }
 }
