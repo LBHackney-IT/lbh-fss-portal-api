cfb37a7efb28971a28b5f77710b7128560673a27
gentex21 <mrpreston_99@yahoo.com>
2020-08-25 15:51:11 +0100

    Use case for the user confirmation route.

---
+++ LBHFSSPortalAPI/V1/UseCase/Interfaces/IConfirmUserUseCase.cs
@@ -0,0 +1,10 @@
+using LBHFSSPortalAPI.V1.Boundary.Requests;
+using LBHFSSPortalAPI.V1.Boundary.Response;
+
+namespace LBHFSSPortalAPI.V1.UseCase.Interfaces
+{
+    public interface IConfirmUserUseCase
+    {
+        UserResponse Execute(UserConfirmRequest confirmRequestData);
+    }
+}
