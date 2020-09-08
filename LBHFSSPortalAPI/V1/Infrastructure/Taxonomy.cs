4fca3d053c5bd0dccdc8d2e4850bd73d525a1dcd
Martin Currie <martin.currie@nudgedigital.co.uk>
2020-09-08 14:12:04 +0100

    Merged db schema changes made within the platform API project into this project

--- LBHFSSPortalAPI/V1/Infrastructure/Taxonomy.cs
+++ LBHFSSPortalAPI/V1/Infrastructure/Taxonomy.cs
@@ -2,12 +2,13 @@ using System;
 
 namespace LBHFSSPortalAPI.V1.Infrastructure
 {
-    public partial class Taxonomies
+    public partial class Taxonomy
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public int? ParentId { get; set; }
         public string Vocabulary { get; set; }
+        public int Weight { get; set; }
         public DateTime? CreatedAt { get; set; }
     }
 }
---
+++ LBHFSSPortalAPI/V1/Infrastructure/User.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+namespace LBHFSSPortalAPI.V1.Infrastructure
+{
+    public class User
+    {
+        public User()
+        {
+            ServiceRevisionsAuthor = new HashSet<ServiceRevision>();
+            ServiceRevisionsReviewerU = new HashSet<ServiceRevision>();
+            Sessions = new HashSet<Session>();
+            UserOrganizations = new HashSet<UserOrganization>();
+        }
+
+        public int Id { get; set; }
+        public string SubId { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public string Status { get; set; }
+
+        public virtual ICollection<ServiceRevision> ServiceRevisionsAuthor { get; set; }
+        public virtual ICollection<ServiceRevision> ServiceRevisionsReviewerU { get; set; }
+        public virtual ICollection<Session> Sessions { get; set; }
+        public virtual ICollection<UserOrganization> UserOrganizations { get; set; }
+    }
+}
