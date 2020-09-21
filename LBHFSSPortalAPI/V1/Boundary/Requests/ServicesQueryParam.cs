911ae10a29ec397d334224462eb34ac1d1d6eda6
Martin Currie <martin.currie@nudgedigital.co.uk>
2020-09-21 17:53:40 +0100

    Started adding GET services, GET service/{id} and POST service endpoints. Added request and response objects and outlined use case classes.

---
+++ LBHFSSPortalAPI/V1/Boundary/Requests/ServicesQueryParam.cs
@@ -0,0 +1,32 @@
+﻿
+namespace LBHFSSPortalAPI.V1.Boundary.Requests
+{
+    public class ServicesQueryParam
+    {
+        /// <summary>
+        /// Search term to use (searches on [name] column for the MVP)
+        /// </summary>
+        public string Search { get; set; }
+
+        /// <summary>
+        /// The column name by which to sort
+        /// </summary>
+        public string Sort { get; set; }
+
+        /// <summary>
+        /// Sort order; asc, desc
+        /// </summary>
+        public string Direction { get; set; }
+
+        /// <summary>
+        /// Maximum number of records to return
+        /// </summary>
+        public int? Limit { get; set; }
+
+        /// <summary>
+        /// Number of records to skip for pagination
+        /// </summary>
+        public int? Offset { get; set; }
+    }
+}
+
