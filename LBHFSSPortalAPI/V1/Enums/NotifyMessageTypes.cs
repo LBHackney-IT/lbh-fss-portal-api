628cc20dff286d9143a36be33569aea5ca684bfa
LBHSPreston <selwyn.preston@hackney.gov.uk>
2020-10-14 07:58:22 +0100

    Enum for various types of messages

---
+++ LBHFSSPortalAPI/V1/Enums/NotifyMessageTypes.cs
@@ -0,0 +1,10 @@
+namespace LBHFSSPortalAPI.V1.Enums
+{
+    public enum NotifyMessageTypes
+    {
+        Reminder,
+        StatusUpdate,
+        AdminNotification,
+        NotReverified
+    }
+}
