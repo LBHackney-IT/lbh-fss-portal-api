40eef63354b53ed0c8dc1d24604a8f078b8f9f80
LBHSPreston <selwyn.preston@hackney.gov.uk>
2020-10-14 07:59:45 +0100

    add notify configuration

--- LBHFSSPortalAPI/V1/Infrastructure/ConnectionInfo.cs
+++ LBHFSSPortalAPI/V1/Infrastructure/ConnectionInfo.cs
@@ -5,7 +5,7 @@ namespace LBHFSSPortalAPI.V1.Infrastructure
         public string AccessKeyId { get; set; }
         public string SecretAccessKey { get; set; }
         public string ClientId { get; set; }
-
         public string UserPoolId { get; set; }
+        public string NotifyKey { get; set; }
     }
 }
