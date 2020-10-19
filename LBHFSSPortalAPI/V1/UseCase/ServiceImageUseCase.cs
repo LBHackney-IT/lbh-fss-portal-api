9c0643df5cf8158e22fc8cbcbc84ee0dda568aa4
LBHSPreston <selwyn.preston@hackney.gov.uk>
2020-10-19 10:06:52 +0100

    ServiceImage use case for orchestrating actions relating to service image uploads

---
+++ LBHFSSPortalAPI/V1/UseCase/ServiceImageUseCase.cs
@@ -0,0 +1,14 @@
+using System.Threading.Tasks;
+using LBHFSSPortalAPI.V1.Boundary.Requests;
+using LBHFSSPortalAPI.V1.UseCase.Interfaces;
+
+namespace LBHFSSPortalAPI.V1.UseCase
+{
+    public class ServiceImageUseCase : IServiceImageUseCase
+    {
+        public async Task ExecuteCreate(ServiceImageRequest request)
+        {
+
+        }
+    }
+}
