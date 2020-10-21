dc2ef05a02ec48d2b8543bb2bd14c0cf3187150e
LBHSPreston <selwyn.preston@hackney.gov.uk>
2020-10-21 09:50:32 +0100

    applied code formatting.

--- LBHFSSPortalAPI/V1/Gateways/OrganisationsGateway.cs
+++ LBHFSSPortalAPI/V1/Gateways/OrganisationsGateway.cs
@@ -205,7 +205,7 @@ namespace LBHFSSPortalAPI.V1.Gateways
         public void LinkUserToOrganisation(Organisation organisation, User user)
         {
             LoggingHandler.LogInfo($"Linking user {user.Name} to organisation {organisation.Name}");
-            var userOrganisation = new UserOrganisation { UserId = user.Id, OrganisationId = organisation.Id, CreatedAt = DateTime.Now};
+            var userOrganisation = new UserOrganisation { UserId = user.Id, OrganisationId = organisation.Id, CreatedAt = DateTime.Now };
             try
             {
                 Context.UserOrganisations.Add(userOrganisation);
