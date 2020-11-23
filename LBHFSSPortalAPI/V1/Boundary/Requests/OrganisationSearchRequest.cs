e95c5915bdae4b4c7af5fc1564f9eb3cf4e9c161
Matthew Keyworth <Matthew.Keyworth@hackney.gov.uk>
2020-11-23 16:05:54 +0000

    Added filter to gateway so organisations can be filtered by status.

--- LBHFSSPortalAPI/V1/Boundary/Requests/OrganisationSearchRequest.cs
+++ LBHFSSPortalAPI/V1/Boundary/Requests/OrganisationSearchRequest.cs
@@ -3,6 +3,7 @@ namespace LBHFSSPortalAPI.V1.Boundary.Requests
     public class OrganisationSearchRequest
     {
         public string Search { get; set; }
+        public string Status { get; set; }
         public string Sort { get; set; }
         public string Direction { get; set; }
         public int? Limit { get; set; }
