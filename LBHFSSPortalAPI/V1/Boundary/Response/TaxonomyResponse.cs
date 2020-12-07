b5351b08b9b185a62c0375ec24b6a7f594cb2d48
Matthew Keyworth <Matthew.Keyworth@hackney.gov.uk>
2020-12-07 11:52:22 +0000

    changed json value of taxonomy response name = label

--- LBHFSSPortalAPI/V1/Boundary/Response/TaxonomyResponse.cs
+++ LBHFSSPortalAPI/V1/Boundary/Response/TaxonomyResponse.cs
@@ -5,6 +5,7 @@ namespace LBHFSSPortalAPI.V1.Boundary.Response
     public class TaxonomyResponse
     {
         public int Id { get; set; }
+        [JsonPropertyName("label")]
         public string Name { get; set; }
         public string Description { get; set; }
         public string Vocabulary { get; set; }
