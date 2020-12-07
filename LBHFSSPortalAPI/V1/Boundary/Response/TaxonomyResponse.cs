00b1d0cdc52333d420a41b26abc906e70e1388fc
Matthew Keyworth <Matthew.Keyworth@hackney.gov.uk>
2020-12-07 12:16:33 +0000

    added vocabulary_id to taxonomy response for ease of front end use. also removed service_description because it's not used anywhere

--- LBHFSSPortalAPI/V1/Boundary/Response/TaxonomyResponse.cs
+++ LBHFSSPortalAPI/V1/Boundary/Response/TaxonomyResponse.cs
@@ -9,9 +9,7 @@ namespace LBHFSSPortalAPI.V1.Boundary.Response
         public string Name { get; set; }
         public string Description { get; set; }
         public string Vocabulary { get; set; }
+        public int VocabularyId { get; set; }
         public int Weight { get; set; }
-
-        [JsonPropertyName("service_description")]
-        public string ServiceDescription { get; set; }
     }
 }
