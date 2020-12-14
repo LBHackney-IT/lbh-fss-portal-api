006c45dddae84791b203f03c7de21a4d7cba9f1c
Matthew Keyworth <Matthew.Keyworth@hackney.gov.uk>
2020-12-14 14:20:47 +0000

    Added gateway methods for taxonomy CRUD

---
+++ LBHFSSPortalAPI/V1/Gateways/Interfaces/ITaxonomyGateway.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LBHFSSPortalAPI.V1.Boundary.Requests;
+using LBHFSSPortalAPI.V1.Domain;
+using LBHFSSPortalAPI.V1.Infrastructure;
+
+namespace LBHFSSPortalAPI.V1.Gateways.Interfaces
+{
+    public interface ITaxonomyGateway
+    {
+        TaxonomyDomain CreateTaxonomy(Taxonomy taxonomy);
+        TaxonomyDomain GetTaxonomy(int id);
+        void DeleteTaxonomy(int id);
+        TaxonomyDomain PatchTaxonomy(Taxonomy taxonomy);
+    }
+}
