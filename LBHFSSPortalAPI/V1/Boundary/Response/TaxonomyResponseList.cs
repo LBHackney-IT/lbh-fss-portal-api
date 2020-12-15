391ea44e78cd10f5ed4c6613f203d6d9c69ead7c
Matthew Keyworth <Matthew.Keyworth@hackney.gov.uk>
2020-12-15 11:46:35 +0000

    Added CRUD for taxonomies controller

---
+++ LBHFSSPortalAPI/V1/Boundary/Response/TaxonomyResponseList.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LBHFSSPortalAPI.V1.Boundary.Response
+{
+    public class TaxonomyResponseList
+    {
+        public List<TaxonomyResponse> Taxonomies { get; set; }
+    }
+}
