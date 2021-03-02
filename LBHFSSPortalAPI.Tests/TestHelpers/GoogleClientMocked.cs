aa4770bceb4eeb9b5430a035f8ed7c8271dc24b0
LBHSPreston <selwyn.preston@hackney.gov.uk>
2021-03-02 10:31:51 +0000

    migrated mock file to test helpers

--- LBHFSSPortalAPI.Tests/TestHelpers/GoogleClientMocked.cs
+++ LBHFSSPortalAPI.Tests/TestHelpers/GoogleClientMocked.cs
@@ -1,16 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading;
 using System.Threading.Tasks;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Drive.v3;
 using Google.Apis.Services;
-using Google.Apis.Sheets.v4;
-using Google.Apis.Sheets.v4.Data;
-using Google.Apis.Util.Store;
 using LBHFSSPortalAPI.V1.UseCase.Interfaces;
 using File = Google.Apis.Drive.v3.Data.File;
 
